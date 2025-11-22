b1ef7ad9e3aa1479cf8701a11490f0b224d47682
@TanayOnTech <66676249+tanaygoyal1111@users.noreply.github.com>
2025-11-22 14:15:00 +0530

    feat: setup main app component and global styles

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './context/LanguageContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import Favorites from './pages/Favorites';
+import OfflineNotice from './components/OfflineNotice';
+
+function App() {
+  return (
+    <LanguageProvider>
+      <Router>
+        <div className="min-h-screen bg-white text-slate-900 font-sans flex flex-col">
+          <OfflineNotice />
+          <Header />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/category/:id" element={<Category />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </LanguageProvider>
+  );
+}
+
+export default App;
