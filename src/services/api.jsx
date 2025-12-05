708dcc4f55399ac18f4d96dde267bf24a7cc01ee
@TanayOnTech <66676249+tanaygoyal1111@users.noreply.github.com>
2025-12-05 12:53:14 +0530

    Update API key

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = "738a87defb0e42c0c4eedfc56501992b";
+const API_KEY = "9c4ee924819d06879c0f3ea084ef27b3";
 const BASE_URL = "/api";
 
 export const apiClient = axios.create({
